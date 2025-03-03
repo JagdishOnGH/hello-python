8c302077469f2472dab7aba42004162e6fd1a20a
jagdish bhai <jagdissghimire@gmail.com>
2025-03-03 15:40:58 +0545

    Chore: python, Let's join the race

---
+++ basic.py
@@ -0,0 +1,18 @@
+print(f"Hello Jagdish!")
+#one shot python
+#conditions
+age = int(input("How old are you?"))
+
+print(f"I am {age} years old.")
+if(age > 18 and age < 70):
+    print("I am mature.")
+elif (age > 70):
+    print("I am old")
+else:
+    print("I am below 18.")
+
+#Variables
+grade = "IX"
+full_marks = 12.2
+name = "Jaggy"
+
